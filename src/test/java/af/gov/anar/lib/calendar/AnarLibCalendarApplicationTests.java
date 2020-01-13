e07ec892a70d73598e0ab62386425530a5dc578f
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-13 13:03:26 +0430

    calendar utility library for anar framework

---
+++ src/test/java/af/gov/anar/lib/calendar/AnarLibCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnarLibCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
